874a5d60e46c963a86024f5e54e7d9e83653bc5c
Blackcaucasion <lteencwadi@gmail.com>
2019-07-29 09:00:16 +0200

    mynode.js API

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require( 'mongoose');
+
+
+// Routes
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://luntuncwadi:'+ process.env.MONGO_ATLAS_PW+'@mynode-td34y.mongodb.net/test?retryWrites=true&w=majority',{
+   useMongoClient :true
+})
+
+// getting through CORS ERROR
+app.use((req,res,next)=>{ 
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow_Headers','*');
+    if(req.method === 'OPTIONS'){
+        Response.header('Access-Control-Allow-Methods','PUT','POST','PATCH','DELETE','GET');
+        return releaseEvents.status(200).json({});
+    }
+    next();
+})
+
+
+app.use(morgan('dev')); /** automtic logging */
+app.use(bodyParser.urlencoded({extended:false}));  /** reading body content  */
+app.use(bodyParser.json());
+
+
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+ //Error handling 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+module.exports = app; 
